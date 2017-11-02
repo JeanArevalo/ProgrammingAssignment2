0474c4b8afbb0296eb9fd1acd8f818c257586853
JeanArevalo <33299498+JeanArevalo@users.noreply.github.com>
2017-11-01 20:29:16 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,1 +1,52 @@
+# In the following script the inverse of a matrix will be
+# computed through the next two functions.
 
+# The function makeCacheMatrix receives a matrix x
+# and makes the following operations:
+# Get the matrix value
+# Set the matrix value
+# Get the inverse of the matrix
+# Set the inverse of the matrix 
+
+
+makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+}
+
+# The next function returns the inverse of a matrix, first it check
+# that the inverse of the matrix has already been calulated, and after
+# gets the result.
+
+cacheSolve <- function(x, ...) {
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
+}
+
+## Example in R:
+
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
